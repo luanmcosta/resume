208236157715bc6f550f53d9fc3c2e2f7dacf62c
Luan Mineu Costa <luanwebdev@gmail.com>
2020-07-06 00:20:52 -0300

    Initial Commit

---
+++ precache-manifest.ceb8a0e18d1eead82cd22ccbd4903f2e.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a6f42164ed90383b45bd0e9c12987128",
+    "url": "/index.html"
+  },
+  {
+    "revision": "597cc25811f01cf88773",
+    "url": "/static/css/main.0865354e.chunk.css"
+  },
+  {
+    "revision": "6c8b4d206dfe940c55d0",
+    "url": "/static/js/2.95f91897.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.95f91897.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "597cc25811f01cf88773",
+    "url": "/static/js/main.9a6f28e2.chunk.js"
+  },
+  {
+    "revision": "824a87c525fc41ffa25d",
+    "url": "/static/js/runtime-main.078ffda6.js"
+  },
+  {
+    "revision": "60048462a45f5ae39703eb5f8a202759",
+    "url": "/static/media/profile.60048462.jpg"
+  }
+]);
